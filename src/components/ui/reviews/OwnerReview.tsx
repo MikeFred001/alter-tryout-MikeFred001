8f078030447d0323e790849fada40e6a012c11c4
Michael Frederickson <mpfplus@gmail.com>
2025-04-01 17:42:17 -0600

    - Added highlight to key review text. - Added custom colors to tailwind config for gradient highlight.

--- src/components/ui/reviews/OwnerReview.tsx
+++ src/components/ui/reviews/OwnerReview.tsx
@@ -7,7 +7,7 @@ export const OwnerReview: React.FC = () => {
 		<div className="flex flex-col gap-6 border border-alter25-black-c border-opacity-20 rounded-lg p-6">
 			<Typography className="text-alter-black text-lg">Easy process for so many insights</Typography>
 			<Typography className="text-alter-black font-light">
-				Loved the Alter DNA kit experience. The swab process and mailing it back was straightforward and simple. The results were fascinating and actionable. Having someone to walk through the report with me was ideal so I could ask questions real time, but the report itself was very easy to understand.
+				Loved the Alter DNA kit experience. The swab process and mailing it back was straightforward and simple.<span className="p-1 rounded bg-gradient-to-r from-highlight-green-fade-in to-highlight-green-fade-out">The results were fascinating and actionable.</span> Having someone to walk through the report with me was ideal so I could ask questions real time, but the report itself was very easy to understand.
 			</Typography>
 
 			<OwnerSignature ownerName="Kathryn S." verified />
